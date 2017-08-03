710be17d8e4e7764aba4b969d8e59e6ebc7ee527
Lo Hong Ket <hklo@seekasia.com>
2017-08-03 16:08:25 +0800

    Update default.rb
    Get proper s3 file

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -7,15 +7,13 @@ access_key = "#{app['app_source']['user']}"
 secret_key = "#{app['app_source']['password']}"
 s3_data = src_url.split('/').drop(3)
 
-p s3_data
-
 my_items.drop(2)
 aws_s3_file "/#{app_path}/app.jar" do
   owner "deploy"
   group "deploy"
   mode "0755"
   bucket s3_data[0]
-  remote_path s3_data[1]
+  remote_path s3_data.drop(1)..join("/")
   aws_access_key_id access_key
   aws_secret_access_key secret_key
   action :create
