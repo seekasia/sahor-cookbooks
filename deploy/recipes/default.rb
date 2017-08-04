bde9ef658f16f6bba635a0b27cb1662c32896f77
Lo Hong Ket <hklo@seekasia.com>
2017-08-04 12:34:08 +0800

    Update default.rb
    Display deploy node

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -8,6 +8,8 @@ secret_key = "#{app['app_source']['password']}"
 s3_data = src_url.split('/').drop(3)
 
 p app['environment']
+p node["deploy"]
+
 
 aws_s3_file "/#{app_path}/app.jar" do
   owner "deploy"
