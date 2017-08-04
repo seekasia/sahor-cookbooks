f6e58d89c53bba29067aaf725f6a5f1a115f5ac9
Lo Hong Ket <hklo@seekasia.com>
2017-08-04 23:56:25 +0800

    Update default.rb

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -39,7 +39,7 @@ aws_s3_file "/#{app_path}/app.jar" do
 end
 
 execute "run app.jar in directory" do
-  command "java -jar app.jar --spring.profiles.active=#{profile} --spring.application.json=#{app_path.to_json} &"
+  command "java -jar app.jar --spring.profiles.active=#{profile} --spring.application.json=#{arg.to_json} &"
   cwd "/#{app_path}"
   action :run
 end
