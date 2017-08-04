bc01b0e1cc277e82e3562aa268665f16475f33af
Lo Hong Ket <hklo@seekasia.com>
2017-08-04 13:22:57 +0800

    Update default.rb

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -7,7 +7,9 @@ access_key = "#{app['app_source']['user']}"
 secret_key = "#{app['app_source']['password']}"
 s3_data = src_url.split('/').drop(3)
 
-rds_db_instance = search(:aws_opsworks_rds_db_instance).first
+rds_arn = app['data_sources']['arn'].gsub(':', '_')
+
+rds_db_instance = search(rds_arn).first
 
 p rds_db_instance
 p app
