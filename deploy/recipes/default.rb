8900343e005c04e21cfc389b54867c1402030f5d
Lo Hong Ket <hklo@seekasia.com>
2017-08-07 22:38:03 +0800

    Get params from node

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,17 +1,21 @@
 require 'json'
 include_recipe 'aws'
 
-app = search('aws_opsworks_app').first
-app_path = "srv/www/#{app['shortname']}"
+deploy_user = node['deploy']['user'] || 'deploy'
+deploy_group = node['deploy']['group'] || 'deploy'
+paths = node['deploy']['path'] || {}
+
+app = search('aws_opsworks_app','deploy:true').first
 src_url = "#{app['app_source']['url']}"
 access_key = "#{app['app_source']['user']}"
 secret_key = "#{app['app_source']['password']}"
 s3_data = src_url.split('/').drop(3)
+datasource = app['data_sources'][0]
 
 database = {}
 
-if app['data_sources'][0]['type'] == "RdsDbInstance"
-  rds_db_instance = search('aws_opsworks_rds_db_instance').first
+if datasource['type'] == "RdsDbInstance"
+  rds_db_instance = search('aws_opsworks_rds_db_instance', "id:#{datasource['arn'].gsub(':', '_')}").first
   
   database['database'] = {
     'driver' => rds_db_instance['engine'],
@@ -23,12 +27,13 @@ if app['data_sources'][0]['type'] == "RdsDbInstance"
   }
 end
 
+app['environment']['log_path'] = paths['log']
 arg = {'arg' => (database.merge(app['environment']))}
 profile = app['environment']['profile'] || 'stage'
 
-aws_s3_file "/#{app_path}/app.jar" do
-  owner "deploy"
-  group "deploy"
+aws_s3_file "#{paths['app']}/app.jar" do
+  owner deploy_user
+  group deploy_group
   mode "0755"
   bucket s3_data[0]
   remote_path s3_data.drop(1).join("/")
@@ -38,14 +43,14 @@ aws_s3_file "/#{app_path}/app.jar" do
 end
 
 execute "start application" do
-  user "deploy"
-  group "deploy"
-  cwd "/#{app_path}"
+  user deploy_user
+  group deploy_group
+  cwd paths['app']
   command "java -jar app.jar --spring.profiles.active=#{profile} --spring.application.json='#{arg.to_json}' &"
   action :run
 end
 
-Chef::Log.info("********** The app's URL is '#{src_url}' **********")
-Chef::Log.info("********** The app's path is '#{app_path}' **********")
+Chef::Log.info("********** The app's source URL is '#{src_url}' **********")
+Chef::Log.info("********** The app's path is '#{paths['app']}' **********")
 Chef::Log.info("********** The app's s3 data is '#{s3_data}' **********")
 Chef::Log.info("********** The app's access key is '#{access_key}' **********")
