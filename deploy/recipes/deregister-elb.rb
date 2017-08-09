a6aaba84ab02b143a86b66df388fa4f58e01173b
chee.collin <chee.collin@seekasia.com>
2017-08-09 12:27:25 +0800

    update log message

--- deploy/recipes/deregister-elb.rb
+++ deploy/recipes/deregister-elb.rb
@@ -2,4 +2,4 @@ aws_elastic_lb 'Deregister Node from ELB' do
   name node['deploy']['elb']
   action :deregister 
 end
-Chef::Log.info("********** Deregistered '#{node[:opsworks][:instance][:aws_instance_id]}' from '#{node['deploy']['elb']}' **********")
+Chef::Log.info("********** Deregistered instance from '#{node['deploy']['elb']}' **********")
--- deploy/recipes/register-elb.rb
+++ deploy/recipes/register-elb.rb
@@ -2,4 +2,4 @@ aws_elastic_lb 'Register Node to ELB' do
   name node['deploy']['elb']
   action :register
 end
-Chef::Log.info("********** Registered '#{node[:opsworks][:instance][:aws_instance_id]}' to '#{node['deploy']['elb']}' **********")
+Chef::Log.info("********** Registered instance to '#{node['deploy']['elb']}' **********")
